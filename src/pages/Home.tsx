04c1ea95ebb70908e4ac3718086aa3fdac60625b
Faye Skeen <fskeen@gmail.com>
2020-06-27 14:51:00 -0700

    basic files

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Column } from "../components/Layout/Column";
+
+const Home = () => {
+  return (
+    <Column size="half">
+      <h1>Home</h1>
+    </Column>
+  );
+};
+
+export default Home;
