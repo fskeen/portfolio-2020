50f511cc160945570615ee31f6b64d3df8ad6864
Faye Skeen <fskeen@gmail.com>
2020-06-28 16:54:53 -0700

    added dummy projects

---
+++ src/data/projects.ts
@@ -0,0 +1,118 @@
+export const projects = [
+  {
+    title: "Allegiance",
+    image: "https://i.imgur.com/kwjcipE.png",
+    carousel: [
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500"
+    ],
+    primarySkills: ["React.js", "Node.js"],
+    skills: ["HTML", "CSS", "React.js"],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    deployedUrl: "https://www.allegiance17.com/",
+    githubUrl: [
+      "https://github.com/lambda-bw-droom/droom-landing",
+      "https://github.com/Lambda-School-Labs/allegiance-fe"
+    ]
+  },
+  {
+    title: "Allegiance",
+    image: "https://i.imgur.com/CCn2oxi.jpg",
+    carousel: [
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500"
+    ],
+    primarySkills: ["JavaScript", "Node.js"],
+    skills: ["HTML", "CSS", "React.js"],
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+    deployedUrl: "https://www.allegiance17.com/",
+    githubUrl: [
+      "https://github.com/lambda-bw-droom/droom-landing",
+      "https://github.com/Lambda-School-Labs/allegiance-fe"
+    ]
+  },
+  {
+    title: "Allegiance",
+    image: "https://i.imgur.com/kwjcipE.png",
+    carousel: [
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500"
+    ],
+    primarySkills: ["React.js", "JavaScript"],
+    skills: ["HTML", "CSS", "React.js"],
+    description:
+      "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+    deployedUrl: "https://www.allegiance17.com/",
+    githubUrl: [
+      "https://github.com/lambda-bw-droom/droom-landing",
+      "https://github.com/Lambda-School-Labs/allegiance-fe"
+    ]
+  },
+  {
+    title: "Allegiance",
+    image: "https://i.imgur.com/zG2Mj5s.jpg",
+    carousel: [
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500"
+    ],
+    primarySkills: ["React.js", "Node.js"],
+    skills: ["HTML", "CSS", "React.js"],
+    description:
+      "Cake oat cake pudding. Lemon drops sweet marzipan brownie chocolate jelly. Gingerbread jelly-o sweet oat cake pudding. Macaroon marzipan sweet liquorice chupa chups ice cream candy danish. Bear claw marzipan macaroon oat cake candy canes halvah gingerbread. Jelly ice cream marzipan marzipan chocolate tart liquorice.",
+    deployedUrl: "https://www.allegiance17.com/",
+    githubUrl: [
+      "https://github.com/lambda-bw-droom/droom-landing",
+      "https://github.com/Lambda-School-Labs/allegiance-fe"
+    ]
+  },
+  {
+    title: "Allegiance",
+    image: "https://i.imgur.com/SxB3Uky.jpg",
+    carousel: [
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500"
+    ],
+    primarySkills: ["React.js", "Node.js"],
+    skills: ["HTML", "CSS", "React.js"],
+    description:
+      "Fruitcake carrot cake lollipop donut sesame snaps. Sesame snaps gummi bears pastry lemon drops cupcake. Gummi bears liquorice halvah toffee. Pudding soufflé chupa chups. Icing jujubes caramels tootsie roll chocolate marzipan. Pudding halvah chupa chups macaroon cheesecake dessert. Pastry croissant cake bear claw gummies. Sugar plum cake liquorice brownie. Cheesecake cookie jelly tart marzipan toffee tiramisu jelly.",
+    deployedUrl: "https://www.allegiance17.com/",
+    githubUrl: [
+      "https://github.com/lambda-bw-droom/droom-landing",
+      "https://github.com/Lambda-School-Labs/allegiance-fe"
+    ]
+  },
+  {
+    title: "Droom",
+    image: "https://i.imgur.com/jv5FtmK.png",
+    carousel: [
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500",
+      "https://picsum.photos/id/545/800/500"
+    ],
+    primarySkills: ["Javascript"],
+    skills: ["HTML", "CSS", "Javascript", "LESS"],
+    description: "This is my project, I dont want to describe it.",
+    deployedUrl: "https://lambda-bw-droom.github.io/droom-landing/",
+    githubUrl: "https://github.com/lambda-bw-droom/droom-landing"
+  }
+];
