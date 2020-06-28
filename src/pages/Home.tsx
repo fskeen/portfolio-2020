f0ab4e2d694cc89a052f33290418eb117dcb5fcb
Faye Skeen <fskeen@gmail.com>
2020-06-28 16:54:33 -0700

    swapped from bootstrap-y column to grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Section } from "../components/Layout/Section";
 import NavBar from "../components/NavBar/NavBar";
 import "./_Home.scss";
+import WorkList from "../components/WorkList/WorkList";
 
 const Home = () => {
+  const workRef = useRef();
+  const topRef = useRef();
+  const bottomRef = useRef();
+
+  const refs = {
+    top: topRef
+  };
   return (
-    <Section size="full" flex={true} className="home">
-      <Section size="quarter" flex={true} column={true}>
-        <NavBar />
-      </Section>
-      <Section size="three-quarters">
-        <h1>Home</h1>
-      </Section>
-    </Section>
+    <main className="home">
+      <NavBar />
+      {/* <h1>Home</h1> */}
+      <WorkList />
+    </main>
   );
+};
+
+const scrollToRef = (ref: any) => {
+  ref.current.scrollIntoView({ behavior: "smooth" });
 };
 
 export default Home;
