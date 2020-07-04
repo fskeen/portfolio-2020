4b5d59f3834e8b24df05c2cb6e84fa88a4db4019
Faye Skeen <fskeen@gmail.com>
2020-07-04 15:17:38 -0700

    added dummy sections for the intro/about me bit

---
+++ src/components/Header/HeaderImage/HeaderImage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./_HeaderImage.scss";
+
+const HeaderImage = () => {
+  return (
+    <div className="header-image">
+      <span>IMAGE HERE</span>
+    </div>
+  );
+};
+
+export default HeaderImage;
