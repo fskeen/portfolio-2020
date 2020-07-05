a4439721d0059edc0da424919c8b57b2b580f306
Faye Skeen <fskeen@gmail.com>
2020-07-05 15:36:27 -0700

    imported image + added it to top of section

--- src/components/Header/IntroBlurb/IntroBlurb.tsx
+++ src/components/Header/IntroBlurb/IntroBlurb.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import "./_IntroBlurb.scss";
 import IntroSection from "./IntroSection";
 import { blurbs } from "../../../data/intro";
+import farmhouse from "../../../assets/farmer-png-icon-8.png";
 
 const IntroBlurb = (props: any) => {
   if (!props?.intro) return null;
   return (
-    <section className="intro-blurb">
-      {blurbs.map((blurb: any, i: number) => (
-        <IntroSection
-          text={blurb.text}
-          imageSource={blurb.img}
-          alt={blurb.alt}
-        />
-      ))}
-    </section>
+    <div className="top-section">
+      <img src={farmhouse} alt="sdfsdf" className="farmhouse" />
+      <section className="intro-blurb">
+        {blurbs.map((blurb: any, i: number) => (
+          <IntroSection
+            text={blurb.text}
+            imageSource={blurb.img}
+            alt={blurb.alt}
+          />
+        ))}
+      </section>
+    </div>
   );
 };
 
