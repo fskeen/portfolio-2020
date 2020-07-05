7280e7945a9226a4ba2f1919fa68f91b727cf472
Faye Skeen <fskeen@gmail.com>
2020-07-04 20:29:36 -0700

    updated data

---
+++ src/data/intro.ts
@@ -0,0 +1,20 @@
+export const blurbs = [
+  {
+    text:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+    img: "https://picsum.photos/id/545/100/100",
+    alt: ""
+  },
+  {
+    text:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+    img: "https://picsum.photos/id/545/100/100",
+    alt: ""
+  },
+  {
+    text:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+    img: "https://picsum.photos/id/545/100/100",
+    alt: ""
+  }
+];
