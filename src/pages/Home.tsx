272cea20719b0e50f55c46b36d33f1584c83f34a
Faye Skeen <fskeen@gmail.com>
2020-07-04 20:30:45 -0700

    started outlining hidden nav

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Section } from "../components/Layout/Section";
 import NavBar from "../components/NavBar/NavBar";
 import "../styles/imports.scss";
@@ -7,16 +7,33 @@ import WorkList from "../components/WorkList/WorkList";
 import Contact from "./Contact";
 import About from "./About";
 
+const determineHideNav = () => {
+  let scrollMarker = window.innerHeight / 3;
+  if (window.scrollY > scrollMarker) {
+    console.log("marker", scrollMarker);
+    console.log("height", window.innerHeight);
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const Home = () => {
   const workRef = useRef();
   const topRef = useRef();
   const bottomRef = useRef();
-
+  const [scrollHeight, setScrollHeight] = useState(window.innerHeight);
+  const [hideNav, setHideNav] = useState(true);
+  // ! Add scroll listener to hide nav
   const refs = {
     top: topRef
   };
+
   return (
-    <main className="home default-theme">
+    <main
+      className={`home default-theme ${hideNav ? "hidden-nav" : ""}`}
+      onScroll={() => determineHideNav()}
+    >
       <NavBar />
       {/* <h1>Home</h1> */}
       <div className="main-content">
